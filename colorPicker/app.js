cfd67f9bd75d7aa63b74a434613bc519fa8ad7aa
AngelB6 <rodrigobarreraangelromero@gmail.com>
2024-03-20 08:50:43 -0500

    Proyecto picker color terminado

--- colorPicker/app.js
+++ colorPicker/app.js
@@ -2,10 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const pickerColor = document.querySelector("#color-picker");
   const visualizarColor = document.querySelector(".visualizar-color");
   const butonVisualizar = document.querySelector(".btn-visual-color");
+  const hexNom = document.querySelector(".hex-color")
 
   butonVisualizar.addEventListener("click", () => {
     const colorSeleccionado = pickerColor.value;
-    document.querySelector(".hex-color").innerHTML = colorSeleccionado;
+    hexNom.textContent = colorSeleccionado;
     visualizarColor.style.backgroundColor = colorSeleccionado;
   });
 });
