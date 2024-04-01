a93e099c35050c1d4251488f6b29f36f1b69ba98
AngelB6 <rodrigobarreraangelromero@gmail.com>
2024-04-01 16:57:10 -0500

    Principio de burbujeo

--- Temas/burbujeoCaptura/app.js
+++ Temas/burbujeoCaptura/app.js
@@ -1,1 +1,45 @@
-const padre = document.querySelector()
+// Burbujeo hace referencia a la ejecucion de mas de un metodo iniciando desde el elemento hijo hasta el elemento padre
+
+// const padre = document.querySelector(".border-primary");
+// const hijo = document.querySelector(".border-secondary");
+// const nieto = document.querySelector(".border-danger");
+
+// padre.addEventListener("click", () => {
+//   console.log("Me diste click padre");
+// });
+
+// hijo.addEventListener("click", () => {
+//   console.log("Me diste click hijo");
+// });
+
+// nieto.addEventListener("click", () => {
+//   console.log("Me diste click nieto ");
+// });
+
+// Captura hace referencia a la ejecucion de mas de un metodo iniciando desde el elemento padre hasta el elemento hijo. Al contrario del burbujeo
+
+// padre.addEventListener(
+//   "click",
+//   () => {
+//     console.log("Me diste click padre");
+//   },
+//   true
+// );
+
+// hijo.addEventListener(
+//   "click",
+//   () => {
+//     console.log("Me diste click hijo");
+//   },
+//   true
+// );
+
+// nieto.addEventListener(
+//   "click",
+//   () => {
+//     console.log("Me diste click nieto ");
+//   },
+//   true
+// );
+
+// Mismos ejemplos con distinta estructura
