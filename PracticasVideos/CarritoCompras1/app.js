17b3cd4d5043fc76ab3279e59e314fe960af50e9
AngelB6 <rodrigobarreraangelromero@gmail.com>
2024-04-01 08:08:21 -0500

    Carrito de frutas terminado en base al video

--- PracticasVideos/CarritoCompras1/app.js
+++ PracticasVideos/CarritoCompras1/app.js
@@ -14,13 +14,27 @@ const agregarCarrito = (e) => {
     cantidad: 1,
   };
 
+  if (carritoObjeto.hasOwnProperty(producto.titulo)) {
+    producto.cantidad = carritoObjeto[producto.titulo].cantidad + 1;
+  }
+
   carritoObjeto[producto.titulo] = producto;
 
-  console.log(carritoObjeto);
+  pintarCarrito();
 };
 
-const pintarCarrito = () =>{
-    console.log('pintar carrito');
-}
+const pintarCarrito = () => {
+  carrito.textContent = "";
+
+  Object.values(carritoObjeto).forEach((item) => {
+    const clone = template.content.firstElementChild.cloneNode(true);
+    clone.querySelector(".lead").textContent = item.titulo;
+    clone.querySelector(".badge").textContent = item.cantidad;
+
+    fragment.appendChild(clone);
+  });
+
+  carrito.appendChild(fragment);
+};
 
 btnesBotones.forEach((btn) => btn.addEventListener("click", agregarCarrito));
